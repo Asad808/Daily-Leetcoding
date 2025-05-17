b9d0e5086f3a908efba49940c6ba40b507affe6e
Asad Ali <97290150+Asad808@users.noreply.github.com>
2025-05-17 16:18:03 +0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.22%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        arr = []
+        arr2 = []
+        combined1 = 0
+        combined2 = 0
+
+        for num in num1:
+            arr.append(ord(num) - ord("0"))
+        for a in arr:
+            combined1 = combined1*10 + a
+
+        
+        for num in num2:
+            arr2.append(ord(num) - ord("0"))
+        for a in arr2:
+            combined2 = combined2*10 + a
+
+        return str(combined1*combined2)
